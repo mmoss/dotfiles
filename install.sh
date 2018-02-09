d1af5ad4149851a8e5c9ede0500a37f2ebd029dd
Murray M. Moss <mmoss@cainc.com>
2018-02-09 09:07:01 -0500

    Update gitconfig

--- install.sh
+++ install.sh
@@ -13,15 +13,5 @@ ln -sfv "$PWD/zshrc.zsh" ~/.zshrc
 ln -sfv "$PWD/zlogin.zsh" ~/.zlogin
 ln -sfv "$PWD/zsh" ~/.zsh/
 
-# Git aliases
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global alias.s status
-git config --global alias.br branch
-git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
-git config --global alias.type 'cat-file -t'
-git config --global alias.dump 'cat-file -p'
-git config --global alias.amend "commit --amend --no-edit"
-git config --global alias.ammend "commit --amend --no-edit"
-git config --global alias.fpush "push --force-with-lease"
+# Git config
+ln -sfv "$PWD/gitconfig" ~/.gitconfig
